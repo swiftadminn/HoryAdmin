942347a76bffe3b87b16751d7f52aa79e074ccad
swiftadmin <swiftdeveloperr@gmail.com>
2024-09-17 16:47:10 +0300

    baslık degıstı

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nunito } from 'next/font/google';
 
 export const metadata: Metadata = {
     title: {
-        template: '%s | VRISTO - Multipurpose Tailwind Dashboard Template',
-        default: 'VRISTO - Multipurpose Tailwind Dashboard Template',
+        template: '%s | HORNY-ADMIN - Telegram Bot Yönetim Sistemi',
+        default: 'HornyBear Yonetim Paneli',
     },
 };
 const nunito = Nunito({
